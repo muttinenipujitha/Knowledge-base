03b86f63483487d6d46d084e4277e344e396dc1f
pujitha <20b01a12b7@svecw.edu.in>
2026-04-03 12:33:58 +0530

    feat: setup Tailwind CSS and base styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        primaryDark: '#1E1B4B',
+      },
+    },
+  },
+  plugins: [],
+}
